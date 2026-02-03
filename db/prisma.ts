a3aeb1bf0a8142592232f9d4f6109e6d08c2e5d2
Lebahati <lebahatijohn@.com>
2026-02-03 12:25:38 +0300

    fix prisma client import for Neon + Vercel

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,13 +1,13 @@
 import { neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '../lib/generated/prisma/client';
+import { PrismaClient } from '../src/generated/prisma';
 import ws from 'ws';
 
 
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
 // const pool = new Pool({ connectionString });
