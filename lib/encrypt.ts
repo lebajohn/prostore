d5d2d59d44d5fa627ea0ee695ac621fea03ff724
Lebahati <lebahatijohn@.com>
2026-02-17 18:58:00 +0300

    route fixes

--- lib/encrypt.ts
+++ lib/encrypt.ts
@@ -1,17 +1,9 @@
 const encoder = new TextEncoder();
-const getKey = () => {
-  const secret = process.env.ENCRYPTION_KEY;
-  if (!secret) {
-    throw new Error("ENCRYPTION_KEY is not defined");
-  }
-  return new TextEncoder().encode(secret);
-};
-// const key = new TextEncoder().encode(process.env.ENCRYPTION_KEY); // Retrieve key from env var
+const key = new TextEncoder().encode(process.env.ENCRYPTION_KEY); // Retrieve key from env var
 
 // Hash function with key-based encryption
 export const hash = async (plainPassword: string): Promise<string> => {
   const passwordData = encoder.encode(plainPassword);
-  const key = getKey();
 
   const cryptoKey = await crypto.subtle.importKey(
     'raw',
@@ -31,6 +23,7 @@ export const hash = async (plainPassword: string): Promise<string> => {
 export const compare = async (
   plainPassword: string,
   encryptedPassword: string
+
 ): Promise<boolean> => {
   const hashedPassword = await hash(plainPassword);
   return hashedPassword === encryptedPassword;
