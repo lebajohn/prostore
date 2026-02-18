c23699e67c83adade82f7334af3be7b9e42fb928
Lebahati <lebahatijohn@.com>
2026-02-18 18:01:35 +0300

    tailwind update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,18 @@
 
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     '@tailwindcss/postcss': {},
-//   },
-// };
-
-// export default config;
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
   },
 };
 
 export default config;
+
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
